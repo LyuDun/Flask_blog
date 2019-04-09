5853576669b0134f96f3812065863e0bef968543
LvDunn <cqcqhelloworld@gmail.com>
2019-04-09 21:04:59 +0800

    use templates

--- flask/app/views.py
+++ flask/app/views.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route('/index/')
 def index():
     return render_template('index.html')
+
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
