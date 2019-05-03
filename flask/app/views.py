f57f20f24ca97fbb6433afbca550dd12bdea5a1b
LvDunn <cqcqhelloworld@gmail.com>
2019-05-03 14:09:58 +0800

    change article fuc and article style

--- flask/app/views.py
+++ flask/app/views.py
@@ -7,8 +7,6 @@ cp = configparser.ConfigParser()
 cp.read('mysql.cfg')
 USER = cp.get('mysql','user')
 PASSWORD = cp.get('mysql','password')
-conn = pymysql.connect(host='localhost', port=3306, user=USER, passwd=PASSWORD, db='test',charset='utf8')
-cur = conn.cursor()
 
 @app.route('/')
 @app.route('/index/')
@@ -23,6 +21,8 @@ def contact():
 def show_article(id):
     sql = "select  article_path from article_name_table where id=%d"%(id) 
     try:
+        conn = pymysql.connect(host='localhost', port=3306, user=USER, passwd=PASSWORD, db='test',charset='utf8')
+        cur = conn.cursor()
         cur.execute(sql)
         article_path = cur.fetchone() 
         return render_template('article.html',article_html=article_path[0])
